6f93106dcacf40ff0da166cac76441eec6ba6ce0
Stiven <starias@ucm.es>
2020-02-27 00:56:44 +0100

    Implementada la clase resources

--- src/Sources/Resources.cpp
+++ src/Sources/Resources.cpp
@@ -1,1 +1,13 @@
 #include "Resources.h"
+
+vector<Resources::ImageInfo> Resources::imageRoutes{
+	//{TextureId, "./Sources/images/name.png"}
+};
+
+vector<Resources::SoundInfo> Resources::soundRoutes{
+	//{AudioId, "./Sources/audio/name.mp3"}
+};
+
+vector <Resources::MusicInfo> Resources::musicRoutes{
+	//{AudioId, "./Sources/audio/name.mp3"}
+};
--- src/Sources/Resources.h
+++ src/Sources/Resources.h
@@ -1,33 +1,44 @@
 #pragma once
+
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-//Enum para las distintas imagenes del juego
-typedef enum TextureId {
-	//Determina la cantidad de texturas
-	TexSize
-};
-
-//Enum para las distintos audios del juego
-typedef enum AudioId {
-	//Determina la cantidad de audios
-	AudSize
-};
-
-//Vector con las rutas de las imagenes a cargar
-static vector<string> texRoutes {
-	{"Ruta de los recursos a cargar"},
-};
-
-//Vector con las rutas de los audios a cargar
-static vector<string> audRoutes{
-	{"Ruta de los recursos a cargar"},
-};
-
 class Resources
 {
-	static int textureSize() { return TexSize; };
-	static int audioSize() { return AudSize; };
+public:
+	//Enum para las distintas imagenes del juego
+	enum TextureId : int {
+	
+	};
+
+	//Enum para las distintos audios del juego
+	enum AudioId : int{
+		
+	};
+
+	struct ImageInfo {
+		TextureId textureId;
+		string filename;
+	};
+
+	struct SoundInfo {
+		AudioId audioId;
+		string filename;
+	};
+
+	struct MusicInfo {
+		AudioId id;
+		string fileName;
+	};
+
+	//Vector con las rutas de las imagenes a cargar
+	static vector<ImageInfo> imageRoutes;
+
+	//Vector con las rutas de los audios a cargar
+	static vector<SoundInfo> soundRoutes;
+
+	//Vector con las rutas de la música a cargar
+	static vector<MusicInfo> musicRoutes;
 };
