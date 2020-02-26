b2938dbec5b852bbe4af307546bc6f77c3f3b160
dgcerdeiras <48984259+dgcerdeiras@users.noreply.github.com>
2020-02-26 22:41:58 +0100

    Creada carpeta "Source" contenedora de .h y .cpp

--- src/Sources/GameObject.h
+++ src/Sources/GameObject.h
@@ -12,9 +12,10 @@ private:
 protected:
 	Vector2D pos_, scale_;
 public:
-	//GameObject() : pos_({ 0,0 }), scale_(0, 0) {};
+	GameObject() : pos_({ 0,0 }), scale_(0, 0) {};
 	//GameObject(Vector2D pos, Vector2D scale) : pos_(pos), scale_(scale) {};
 	GameObject(Vector2D pos = { 0,0 }, Vector2D scale = {0,0}): pos_(pos),scale_(scale) {}
+	virtual void update() = 0;
 	virtual void logic() = 0;
 };
 
--- src/Sources/Player.h
+++ src/Sources/Player.h
@@ -2,5 +2,9 @@
 #include "Actor.h"
 class Player : public Actor
 {
+public:
+	Player();
+	~Player();
+	void update();
 };
 
