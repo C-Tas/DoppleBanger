46634ebaf6470f7f31f185c626ed071a172e2a62
Amaro Blest <ablest@ucm.es>
2020-03-04 00:29:30 +0100

    ADDED: constructores y métodos básicos

--- src/Sources/Dynamic.h
+++ src/Sources/Dynamic.h
@@ -2,9 +2,21 @@
 #include "Collider.h"
 class Dynamic : public Collider
 {
-protected:
-	//double velX_, velY_;
+private:
 	Vector2D vel_;
+protected:
+	Dynamic() : vel_({ 0,0 }) {};
+	Dynamic(Vector2D vel) : vel_(vel) {};
+	Dynamic(Dynamic& other): vel_(other.vel_) {};
+	Dynamic(Dynamic&& other)noexcept : vel_(other.vel_) {};
 public:
+#pragma region getters
+	const Vector2D* getVel() { return &vel_; };
+#pragma endregion
+#pragma region setters
+	void setVel(Vector2D vel) { vel_ = vel; };
+	void setVelX(double velX) { vel_.setX(velX); };
+	void setVelY(double velY) { vel_.setY(velY); };
+#pragma endregion
 };
 
