424ac0edc0a04fc9614265bbc93f7aa205093795
Amaro Blest <ablest@ucm.es>
2020-03-04 17:25:04 +0100

    FIXED: agregado metodo move y cambiado vel por dir

--- src/Sources/Dynamic.h
+++ src/Sources/Dynamic.h
@@ -2,21 +2,13 @@
 #include "Collider.h"
 class Dynamic : public Collider
 {
-private:
-	Vector2D vel_;
 protected:
-	Dynamic() : vel_({ 0,0 }) {};
-	Dynamic(Vector2D vel) : vel_(vel) {};
-	Dynamic(Dynamic& other): vel_(other.vel_) {};
-	Dynamic(Dynamic&& other)noexcept : vel_(other.vel_) {};
-public:
-#pragma region getters
-	const Vector2D* getVel() { return &vel_; };
-#pragma endregion
-#pragma region setters
-	void setVel(Vector2D vel) { vel_ = vel; };
-	void setVelX(double velX) { vel_.setX(velX); };
-	void setVelY(double velY) { vel_.setY(velY); };
-#pragma endregion
+	Vector2D dir_;				//Vector que representa la dirección
+	double speed_;				//Representa la velocidad del cuerpo
+	Dynamic() : dir_({ 0,0 }), speed_(0) {};
+	Dynamic(Vector2D vel, double speed) : dir_(vel), speed_(speed) {};
+	Dynamic(Dynamic& other): dir_(other.dir_),speed_(other.speed_) {};
+	Dynamic(Dynamic&& other)noexcept : dir_(other.dir_), speed_(other.speed_) {};
+	void move(Point2D target);
 };
 
