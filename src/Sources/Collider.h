90efab616ef447f7d29dc1d019332aca31767b0d
Amaro Blest <ablest@ucm.es>
2020-03-04 17:41:03 +0100

    FIXED: pequeños cambios

--- src/Sources/Collider.h
+++ src/Sources/Collider.h
@@ -11,5 +11,8 @@ protected:
 	Collider(const Collider& other) :collisionArea_(other.collisionArea_) {};
 	Collider(const Collider&& other)noexcept : collisionArea_(other.collisionArea_) {};
 	virtual void onCollider() = 0;
+public:
+	const SDL_Rect* getCollider() { return collisionArea_; };
+	void setCollider(SDL_Rect* newRect) { collisionArea_ = newRect; };
 };
 
