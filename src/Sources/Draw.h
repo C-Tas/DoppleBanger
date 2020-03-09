f72b3ca6143342a5e6a070db5e98ec57a5f0f25c
Leire Osés <leoses@ucm.es>
2020-03-10 00:27:23 +0100

    Ya se puede hacer click en los botones
    El problema venía porque getDestiny no daba las coordenadas que debería (puesto que en la versión antigua de Draw, destiny_ era un puntero en vez de un "objeto").
    
    Actualizada esta rama con la versión de Draw más actual

--- src/Sources/Draw.h
+++ src/Sources/Draw.h
@@ -1,35 +1,64 @@
-#pragma once
+﻿#pragma once
 #include "GameObject.h"
 #include "Texture.h"
-
+#include "SDL.h"
 
 
 class Draw : public GameObject
 {
 protected:
 	Texture* texture_;
-	SDL_Rect* destiny_;
-	SDL_Rect* frame_;
-	Draw() :texture_(nullptr), destiny_(nullptr), frame_(nullptr) {};
-	Draw(Texture* texture, SDL_Rect* destiny, Point2D pos, Vector2D scale) :
-		GameObject(pos, scale), texture_(texture), destiny_(destiny), frame_(nullptr) {};
-	Draw(Texture* texture, SDL_Rect* destiny, SDL_Rect* frame, Point2D pos, Vector2D scale) :
-		GameObject(pos, scale), frame_(frame), texture_(texture), destiny_(destiny) {};
-	Draw(const Draw& other) : GameObject(other.pos_, other.scale_), 
+	SDL_Rect destiny_;
+	SDL_Rect frame_;
+	Vector2D visPos_;
+
+	Draw() : texture_(nullptr) {};
+
+	Draw( Texture* texture, Point2D pos, Vector2D scale) :
+		GameObject( pos, scale), texture_(texture) {
+		destiny_.x = (int)pos.getX();
+		destiny_.y = (int)pos.getY();
+		destiny_.w = (int)scale.getX();
+		destiny_.h = (int)scale.getY();
+	};
+
+	Draw( Texture* texture, SDL_Rect* frame, Point2D pos, Vector2D scale) :
+		GameObject( pos, scale), texture_(texture) {
+		destiny_.x = (int)pos.getX();
+		destiny_.y = (int)pos.getY();
+		destiny_.w = (int)scale.getX();
+		destiny_.h = (int)scale.getY();
+	};
+
+	Draw(const Draw& other) : GameObject( other.pos_, other.scale_),
 		texture_(other.texture_), destiny_(other.destiny_), frame_(other.frame_) {};
-	Draw(const Draw&& other)noexcept : GameObject(other.pos_, other.scale_),	
+
+	Draw(const Draw&& other)noexcept : GameObject( other.pos_, other.scale_),
 		texture_(other.texture_), destiny_(other.destiny_), frame_(other.frame_) {};
-	virtual ~Draw() { delete destiny_, frame_; };
+
+	virtual ~Draw() {};
+
 public:
-	const virtual void draw() { texture_->render(*destiny_, SDL_FLIP_NONE); };
+	const virtual void draw() { texture_->render(getDestiny(), SDL_FLIP_NONE); };
+	//Devuelve la posici�n "visual" del objeto
+	//Cuando se mueva un objeto no se mira su posici�n superior izquierda, sino sus pies.
+	void updateVisPos() { visPos_.setVec(Vector2D(pos_.getX() + (scale_.getX() / 2), pos_.getY() + (scale_.getY() * 0.8))); };
+
+
 #pragma region getters
-	const virtual SDL_Rect getDestiny() { return *destiny_; };
+	const virtual SDL_Rect getDestiny() {
+		destiny_.x = (int)pos_.getX();
+		destiny_.y = (int)pos_.getY();
+		destiny_.w = (int)scale_.getX();
+		destiny_.h = (int)scale_.getY();
+		return destiny_;
+	};
 #pragma endregion
 
+
 #pragma region setters
-	void setFrame(SDL_Rect* frame) { frame_ = frame; };
-	void setDestiny(SDL_Rect* destiny) { destiny_ = destiny; };
+	void setFrame(SDL_Rect frame) { frame_ = frame; };
+	void setDestiny(SDL_Rect destiny) { destiny_ = destiny; };
 	void setTexture(Texture* texture) { texture_ = texture; };
 #pragma endregion
 };
-
