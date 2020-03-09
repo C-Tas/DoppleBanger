23d749876778569d6f0e23d5cb814dcf3569b0ca
georgimednikov <gmedniko@ucm.es>
2020-03-09 11:03:25 +0100

    Añadida clase item y container

--- src/Sources/Item.h
+++ src/Sources/Item.h
@@ -1,6 +1,25 @@
 #pragma once
+#include "Texture.h"
+
 
 class Item
 {
-	//Copiar clase item de Georgi
+private:
+	string itemName_, itemDescription_;
+	Texture* itemTexture_;
+	double itemQuantity_, itemPrice_;
+
+public:
+
+	void setItemDescription(string desc) { itemDescription_ = desc; };
+	void setItemName(string name) { itemName_ = name; };
+	void setItemTexture(Texture* tex) { itemTexture_ = tex; };
+	void setItemQuantity(double d) { itemQuantity_ = d; };
+	void setItemPrice(double name) { itemPrice_ = name; };
+
+	string getItemDescription() { return itemDescription_; };
+	string getItemName() { return itemName_; };
+	Texture* getItemTexture() { return itemTexture_; };
+	double getItemQuantity() { return itemQuantity_; };
+	double getItemPrice() { return itemPrice_; };
 };
