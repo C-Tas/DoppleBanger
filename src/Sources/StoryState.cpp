3e0d17c62766631ea2a8bf68d451892cabf407b5
Leire Osés <leoses@ucm.es>
2020-03-12 20:55:27 +0100

    Añadido botón para saltarse la secuencia de escenas

--- src/Sources/StoryState.cpp
+++ src/Sources/StoryState.cpp
@@ -1,23 +1,37 @@
 #include "StoryState.h"
 #include "PlayState.h"
-void StoryState::goToGame() {
-	app_->getStateMachine()->changeState(new PlayState(app_));
+void StoryState::goToGame(Application* app) {
+	app->getStateMachine()->changeState(new PlayState(app));
 }
 
 void StoryState::draw() const {
+
+	GameState::draw();
+
 	//Dibujamos en toda la pantalla, tomando como origen el (0,0)
 	scenes[currentScene].sceneTexture->render({ 0,0,app_->getWindowWidth(), app_->getWindowHeight() });
 }
 
 void StoryState::update() {
+
+	bool stateChanged = false; //Booleano para comprobar si ya se ha cambiado de estado
+
 	//Comprobamos si la duración de la escena ya se ha completado
 	if (SDL_GetTicks() - timeSinceUpdate > scenes[currentScene].duration) {
 		timeSinceUpdate = SDL_GetTicks();
 		//Pasamos a la siguiente
 		currentScene++;
 		//Si ya no hay más escenas, pasamos al juego
-		if (currentScene >= scenes.size()) goToGame();
+		if (currentScene >= scenes.size()) {
+			goToGame(app_); //Go to PlayState
+			stateChanged = true;
+		}
 	}
+
+	//Si ya se ha acabado la secuencia de escenas, no se procesa el estado de los objetos del juego
+	//(pq estaría en PlayState ya)
+	if(!stateChanged)GameState::update();
+	
 }
 
 void StoryState::initState() {
@@ -26,5 +40,8 @@ void StoryState::initState() {
 	scenes[0] = { 2000, app_->getTextureManager()->getTexture(Resources::TextureId::Scene1) };
 	scenes[1] = { 3000, app_->getTextureManager()->getTexture(Resources::TextureId::Scene2) };
 	scenes[2] = { 2000, app_->getTextureManager()->getTexture(Resources::TextureId::Scene3) };
+
+	//Botón para saltarse la secuencia de escenas
+	createButton(app_->getTextureManager()->getTexture(Resources::TextureId::Timon), { 10,10 }, { 50,50 }, goToGame, app_);
 }
 
--- src/Sources/StoryState.h
+++ src/Sources/StoryState.h
@@ -4,6 +4,7 @@
 
 using uint = unsigned int;
 
+//<summary>Struct que define cada una de las escenas (imagen a mostrar y duración de la misma)</summary>
 struct Scene {
 	Uint32 duration;
 	Texture* sceneTexture;
@@ -23,7 +24,7 @@ public:
 	virtual void update();
 
 	///<summary>Método para pasar al juego </summary>
-	void goToGame();
+	static void goToGame(Application* app);
 private:
 	///<summary>Numero de escenas que cuentan la historia del juego</summary>
 	static const uint nScenes = 3;
@@ -34,7 +35,8 @@ private:
 	///<summary>Tiempo desde el ultimo cambio de escena</summary>
 	Uint32 timeSinceUpdate;
 
-	///<summary>Método auxiliar para inicializar el estado</summary>
+	///<summary>Método auxiliar para inicializar el estado, asigna las texturas de las escenas, el tiempo que dura cada una y
+	/// un botón para saltar directamente al juego</summary>
 	void initState();
 };
 
