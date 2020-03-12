3953bc0a07645b4581a7116da3b2ce602b9b7a2d
Leire Osés <leoses@ucm.es>
2020-03-12 19:12:31 +0100

    Implementada funcionalidad del StoryState

--- src/Sources/StoryState.cpp
+++ src/Sources/StoryState.cpp
@@ -3,3 +3,28 @@
 void StoryState::goToGame() {
 	app_->getStateMachine()->changeState(new PlayState(app_));
 }
+
+void StoryState::draw() const {
+	//Dibujamos en toda la pantalla, tomando como origen el (0,0)
+	scenes[currentScene].sceneTexture->render({ 0,0,app_->getWindowWidth(), app_->getWindowHeight() });
+}
+
+void StoryState::update() {
+	//Comprobamos si la duración de la escena ya se ha completado
+	if (SDL_GetTicks() - timeSinceUpdate > scenes[currentScene].duration) {
+		timeSinceUpdate = SDL_GetTicks();
+		//Pasamos a la siguiente
+		currentScene++;
+		//Si ya no hay más escenas, pasamos al juego
+		if (currentScene >= scenes.size()) goToGame();
+	}
+}
+
+void StoryState::initState() {
+	//De momento estoy metiendo los fondos de esta forma, pero si a alguien se le ocurre
+	//como hacerlo de una forma mejor se aceptan sugerencias
+	scenes[0] = { 2000, app_->getTextureManager()->getTexture(Resources::TextureId::Scene1) };
+	scenes[1] = { 3000, app_->getTextureManager()->getTexture(Resources::TextureId::Scene2) };
+	scenes[2] = { 2000, app_->getTextureManager()->getTexture(Resources::TextureId::Scene3) };
+}
+
--- src/Sources/StoryState.h
+++ src/Sources/StoryState.h
@@ -1,11 +1,40 @@
 #pragma once
+#include <array>
 #include "GameState.h"
+
+using uint = unsigned int;
+
+struct Scene {
+	Uint32 duration;
+	Texture* sceneTexture;
+};
+
 class StoryState :
 	public GameState
 {
 public:
-	StoryState(Application* app = nullptr) :GameState(app) {};
+	///<summary>Constructora de la clase StoryState</summary>
+	StoryState(Application* app = nullptr) :GameState(app) { initState(); timeSinceUpdate = SDL_GetTicks(); };
+	///<summary>Destructora</summary>
 	virtual ~StoryState() {};
+
+	///<summary>Metodos redefinifos de la clase padre</summary>
+	virtual void draw() const;
+	virtual void update();
+
+	///<summary>Método para pasar al juego </summary>
 	void goToGame();
+private:
+	///<summary>Numero de escenas que cuentan la historia del juego</summary>
+	static const uint nScenes = 3;
+	///<summary>Array que contiene las información (Textura y duración) de las distintas escenas</summary>
+	array<Scene, nScenes> scenes;
+	///<summary>Contador de la escena en la que nos encontramos</summary>
+	int currentScene = 0;
+	///<summary>Tiempo desde el ultimo cambio de escena</summary>
+	Uint32 timeSinceUpdate;
+
+	///<summary>Método auxiliar para inicializar el estado</summary>
+	void initState();
 };
 
