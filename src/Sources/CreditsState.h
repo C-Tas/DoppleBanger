21c0621443a7c1950f3415b9a742633d4d135635
Amaro Blest <ablest@ucm.es>
2020-03-13 21:24:57 +0100

    Added: funcionaminto basico para creditstate

--- src/Sources/CreditsState.h
+++ src/Sources/CreditsState.h
@@ -8,7 +8,7 @@ public:
 	CreditsState(Application* app = nullptr) :GameState(app) { initCreditState(); };
 	///<summary>Destructora</summary>
 	virtual ~CreditsState() {};
-
+	///<summary>Inicializa la escena creditState</summary>
 	void initCreditState();
 	///<summary>Metodo para pasar a los botones de este estado, vuelve al anterior</summary>
 	static void backToPreviousState(Application* app);
