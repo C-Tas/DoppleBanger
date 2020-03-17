9dce7a63655380ef2b6b82c956479b75e77d03aa
Agus <amoure@gmail.com>
2020-03-17 21:11:05 +0100

    Eliminadas las variables sin usar que se me pasaron en el amterior commit

--- src/Sources/Enemy.h
+++ src/Sources/Enemy.h
@@ -3,23 +3,19 @@
 using namespace std;
 class Enemy : public Actor
 {
-private:
-protected:
-	int damage_;
-	int health_;
 public:
-	//<summary>Constructor tanto por defecto como por contenido si no se le pasan valores serán los puestos, si se le pasan valores los editara</summary>
+	//<summary>Constructor tanto por defecto como por contenido si no se le pasan valores serï¿½n los puestos, si se le pasan valores los editara</summary>
 	Enemy(Application* app = nullptr, Texture* texture = nullptr, Vector2D pos = { 0,0 }, Vector2D scale = { 0,0 }, SDL_Rect collisionArea = { 0,0,0,0 }, Stats stats = {}, SDL_Rect frame = { 0,0,0,0 }, int numberFrames = 0) :Actor(app, texture, pos, scale, collisionArea, frame, numberFrames) { currStats_ = stats; };
 	//<summary>Constructor por copia</summary>
-	Enemy(Enemy& other) :Actor(other.app_, other.texture_, other.pos_, other.scale_, other.collisionArea_),damage_(other.damage_),health_(other.health_) {};
+	Enemy(Enemy& other) :Actor(other.app_, other.texture_, other.pos_, other.scale_, other.collisionArea_) {currStats_=other.currStats_};
 	//<summary>Constructor por movimiento<summary>
-	Enemy(Enemy&& other)noexcept :Actor(other.app_, other.texture_, other.pos_, other.scale_, other.collisionArea_),damage_(other.damage_), health_(other.health_) {};
+	Enemy(Enemy&& other)noexcept :Actor(other.app_, other.texture_, other.pos_, other.scale_, other.collisionArea_) {currStats_=other.currStats_};
 	//<summary>Destructor</summary>
 	virtual ~Enemy() {};
-	//<summary>Metodo que llevará la logica de nuestros enemigos cada enemigo tiene un comportamiento diferente por los que lo unico común es el cambio del frame de la animación </summary>
+	//<summary>Metodo que llevarï¿½ la logica de nuestros enemigos cada enemigo tiene un comportamiento diferente por los que lo unico comï¿½n es el cambio del frame de la animaciï¿½n </summary>
 	virtual bool update() { updateFrame();return false; };
 	virtual void onCollider() {};
-	//<summary>Metodo que renderizará nuestro enemigos, (se ha hecho suponiendo que todos tienen animación </summary>
+	//<summary>Metodo que renderizarï¿½ nuestro enemigos, (se ha hecho suponiendo que todos tienen animaciï¿½n </summary>
 	virtual void draw() const { texture_->render(destiny_, frame_); };
 
 };
