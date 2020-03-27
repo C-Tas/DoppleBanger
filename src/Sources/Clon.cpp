a0feaa094fced7d24ae06802c68eae7385029b23
Daniel González Cerdeiras <daniel20@ucm.es>
2020-03-27 17:50:40 +0100

    Arreglado bug de ataque global del clon

--- src/Sources/Clon.cpp
+++ src/Sources/Clon.cpp
@@ -4,7 +4,7 @@
 bool Clon::update()
 {
 	if ((SDL_GetTicks() - spawnTime_) / 1000 < duration_) {
-		if (ad_ > 0 && (objective_ == nullptr || !objective_->isAlive() || Vector2D().magnitude() > range_))
+		if (ad_ > 0 && (objective_ == nullptr || !objective_->isAlive() || Vector2D(abs(objective_->getPosX() - pos_.getX()), abs(objective_->getPosY() - pos_.getY())).magnitude() > range_))
 			objective_ = static_cast<PlayState*>(app_->getStateMachine()->getState())->findClosestEnemy(pos_);
 
 		else if (ad_ > 0 && ((SDL_GetTicks() - meleeTime_) / 1000) > meleeRate_)
