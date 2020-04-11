3b9c568523aafa7c7384a1c5e1ee4f6a75d52bba
OskarFreestyle <oscafe01@ucm.es>
2020-04-11 17:25:22 +0200

    Añadido SkillState funcional
    .

--- src/Sources/SkillButton.cpp
+++ src/Sources/SkillButton.cpp
@@ -2,7 +2,7 @@
 
 bool SkillButton::update()
 {
-	SDL_Point mouse = { handleEvents_->getMousePos().getX(), handleEvents_->getMousePos().getY() };
+	SDL_Point mouse = { handleEvents_->getRealMousePos().getX(), handleEvents_->getRealMousePos().getY() };
 
 	if (unlocked_ && SDL_PointInRect(&mouse, &getDestiny()) && handleEvents_->getMouseButtonState(HandleEvents::MOUSEBUTTON::LEFT)) {
 		callbackSkill_(app_, this);
