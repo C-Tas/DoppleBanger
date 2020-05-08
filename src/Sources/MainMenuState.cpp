dbd846234fbad6ef49bae63b73fe612b3b4aaab9
Stiven Arias <starias@ucm.es>
2020-05-08 13:55:24 +0200

    Cambios en la posición del Botón de Controles y Créditos - MainMenuState

--- src/Sources/MainMenuState.cpp
+++ src/Sources/MainMenuState.cpp
@@ -64,14 +64,14 @@ void MainMenuState::createButtons() {
 			{ (winWidth / 2) + button_w / 2, (winHeight / 2) },
 			{ button_w,button_h }, goLoadState, this);
 	}
-	//creamos el boton para ir a los creditos
-	createBeerButton(app_, app_->getTextureManager()->getTexture(Resources::TextureId::CreditsButton),
-		{ double(app_->getWindowWidth() / 2) - button_w * 1.5, double(app_->getWindowHeight() / 2) + button_h * 1.2 },
-		{ button_w  ,button_h }, goCreditsState, this);
 	//creamos el boton para ir a los controles
 	createBeerButton(app_, app_->getTextureManager()->getTexture(Resources::TextureId::ControlsButton),
-		{ (winWidth / 2) + button_w / 2, double(app_->getWindowHeight() / 2) + button_h * 1.2 },
+		{ double(app_->getWindowWidth() / 2) - button_w * 1.5, double(app_->getWindowHeight() / 2) + button_h * 1.2 },
 		{ button_w  ,button_h }, goControlState, this);
+	//creamos el boton para ir a los creditos
+	createBeerButton(app_, app_->getTextureManager()->getTexture(Resources::TextureId::CreditsButton),
+		{ (winWidth / 2) + button_w / 2, double(app_->getWindowHeight() / 2) + button_h * 1.2 },
+		{ button_w  ,button_h }, goCreditsState, this);
 	//Boton para salir del juego
 	createBeerButton(app_, app_->getTextureManager()->getTexture(Resources::TextureId::ExitButton),
 		{ (winWidth / 2) - button_w / 2, (winHeight / 2) + button_h * 2.4 },
